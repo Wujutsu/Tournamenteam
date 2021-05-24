05ee60a5ff23627044c72b18e6b86ee5f197fcc8
Wujutsu <kevin.duballet@gmail.com>
2021-05-24 14:49:22 +0200

    création authentification

---
+++ src/Controller/EventController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="redirect_event")
+     */
+    public function redict_event()
+    {
+        return $this->redirectToRoute("event");
+    }
+
+    /**
+     * @Route("/event", name="event")
+     */
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+}
